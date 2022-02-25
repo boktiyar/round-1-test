dc678f92ba05fb168b2e6f7621915edfd8f0e896
Boktiyar Hossain Molla <94525146+boktiyar@users.noreply.github.com>
2022-02-26 01:28:30 +0530

    single page design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'round-1-test';
+  boktiyar=[
+    {
+      compan: "Software Engineer",
+      company: "Krinati Solution Pvt Ltd . Full-time",
+      startDate: "Mar 2021 - Present . 10 month",
+      localtion: "Mumbai,Maharashtra,India"
+
+    },
+    {
+      compan: "B-Learn B-Earn Solution",
+      company: "",
+      startDate: "Full -time . 1 yr 7 month",
+      loca:"Full- Stack Engineer",
+      endDate: "Mar 2020 - Apr 2021 . 1 yr 2 month",
+      locaa: "Delhi,India",
+      sift:"Frontend Developer",
+      gett:"Nov 2019 - Mar 2020 . 5 month",
+      locaaa:"Delhi,India",
+    },
+    {
+      compan: "Root Level",
+      company: "Mattermost . part-time",
+      startDate: "Aug 2020 - Dec 2020 . 5 month",
+      locaa: "Kolkata,West Bengal,India",
+
+     
+    },
+    
+  ]
 }
